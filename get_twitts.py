fc1701c07768d9f11010976cb66a5a9eaf942f1d
juancastillo <juan.castillog@sansano.usm.cl>
2020-02-25 20:10:27 -0300

    initial commit

---
+++ get_twitts.py
@@ -0,0 +1,22 @@
+import twint
+
+
+def get_twitts(hashtag, file_name, since_date, until_date, format = '', limit = 100, custom = []):    
+    c = twint.Config()
+    if format:
+        c.Format = format
+    if len(custom):
+        c.Custom['tweet']= custom
+    c.Search = hashtag 
+    c.Store_csv = True
+    c.Since = since_date
+    c.Until = until_date
+    c.Limit = limit
+    c.Output = file_name
+    c.Links = False
+    twint.run.Search(c)
+
+
+custom_retrieve = ['date', 'timezone', 'user_id', 'username', 'tweet', 'hashtags', 'geo', 'source']
+
+get_twitts('chile', 'test_data.csv', since_date = "2019-10-18 19:00:00", custom=custom_retrieve, until_date = "2019-10-19 23:59:59" , limit=10000)
