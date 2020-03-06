3ba604a55ac2db672b42c06c8c28128953953129
yeip1 <juan.castillog@sansano.usm.cl>
2020-03-06 01:21:06 -0300

    add tentative hashtags

--- get_twitts.py
+++ get_twitts.py
@@ -3,6 +3,7 @@ import os
 import glob 
 import pandas as pd
 
+hashtags = ['#chile', '#Chile', '#ChileViolaLosDerechosHumanos', '#ChileViolatesHumanRights', '#Noestamosenguerra', '#Piñerarenuncia', '#lamarchamásgrandedeChile']
 
 def get_twitts(hashtag, file_name, since_date, until_date, format = '', limit = 100, custom = []):    
     c = twint.Config()
@@ -40,24 +41,24 @@ def merge_files():
 def get(hashtag, limit=1000):
     custom_retrieve = ['date', 'timezone', 'user_id', 'username', 'tweet', 'hashtags', 'geo', 'source']
     dates = ["2019-10-18", '2019-10-19', '2019-10-20', '2019-10-25', '2019-11-11']
-    
-    for d in dates:
-        split = d.split('-')
-        print(split)
-        since_day = int(split[-1])
-        until_day = since_day +  1
-        print(since_day)
-        print(until_day)
-        since_day = str(since_day) if len(str(since_day)) > 1 else f'0{since_day}'
-        until_day = str(until_day) if len(str(until_day)) > 1 else f'0{until_day}'
-        print(since_day)
-        print(until_day)
-        since_date = f'{split[0]}-{split[1]}-{since_day}'
-        until_date = f'{split[0]}-{split[1]}-{until_day}'
-        
-        file_name = f'./temp_files/{hashtag}-{d}.csv'
-        get_twitts(hashtag=hashtag, file_name=file_name, since_date=since_date, until_date=until_date, custom=custom_retrieve,limit=limit)
+    for h in hashtags:    
+        for d in dates:
+            split = d.split('-')
+            print(split)
+            since_day = int(split[-1])
+            until_day = since_day +  1
+            print(since_day)
+            print(until_day)
+            since_day = str(since_day) if len(str(since_day)) > 1 else f'0{since_day}'
+            until_day = str(until_day) if len(str(until_day)) > 1 else f'0{until_day}'
+            print(since_day)
+            print(until_day)
+            since_date = f'{split[0]}-{split[1]}-{since_day}'
+            until_date = f'{split[0]}-{split[1]}-{until_day}'
+            
+            file_name = f'./temp_files/{h}-{d}.csv'
+            get_twitts(hashtag=h, file_name=file_name, since_date=since_date, until_date=until_date, custom=custom_retrieve,limit=limit)
 
-get('chile', limit=1000)
+get(['#Chile'], limit=1000)
 #merge_files()
 
